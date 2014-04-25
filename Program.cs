041da23b19fe5daa126d7539c6dc36ba444b561d
Collin Sauve <collin.sauve@oracle.com>
2014-04-25 19:28:31 -0400

    success!

--- Program.cs
+++ Program.cs
@@ -6,8 +6,7 @@ namespace IISRedirection
     public class Program
     {
 
-        private const string xmlFormat = @"
-<configuration>
+        private const string xmlFormat = @"<configuration>
     <configSections>
         <section name=""configurationRedirection"" />
     </configSections>
@@ -18,10 +17,8 @@ namespace IISRedirection
     </configProtectedData>
     <configurationRedirection enabled=""true"" path=""{0}"" />
 </configuration>";
-        
-        private const string TargetPath32 = @"%windir%\system32\inetsrv\config\redirection.config";
-        private const string TargetPath64 = @"%windir%\sysWOW64\inetsrv\config\redirection.config";
-        
+
+        private const string TargetPath = @"%windir%\system32\inetsrv\config\redirection.config";
 
         public static int Main(string[] args)
         {
@@ -32,14 +29,16 @@ namespace IISRedirection
             }
 
             var tempFile = Path.GetTempFileName();
+            var path = args[0].TrimEnd(new[] { '"', ' ' });
             using (var stream = File.CreateText(tempFile))
             {
-                stream.Write(string.Format(xmlFormat, args[0]));
+                
+                stream.Write(xmlFormat, path);
             }
-            File.Copy(tempFile, Environment.ExpandEnvironmentVariables(TargetPath32), true);
-            File.Copy(tempFile, Environment.ExpandEnvironmentVariables(TargetPath64), true);
+
+            File.Copy(tempFile, Environment.ExpandEnvironmentVariables(TargetPath), true);
             File.Delete(tempFile);
-            
+        
             return 0;
         }
     }
